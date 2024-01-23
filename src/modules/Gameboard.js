116be1ea223e3556aa9a674f7dd09ffec54fe3d7
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-01-23 22:58:18 +0100

    LegalPlacement method changes
    I've made changes to my legal placement method in my Gameboard.js so that
    it can check if cell on the board contains ship object if so the ship will
    not be placed. Also, I've disabled placement of ship on nearby cells.

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,3 +1,5 @@
+const Ship = require("./Ship");
+
 class GameBoard {
   constructor() {
     this.grid = Array(10)
@@ -7,7 +9,35 @@ class GameBoard {
   }
 
   legalPlacement(ship, legalCoordinate, direction) {
+    let xCoordinate = legalCoordinate[1];
+    let yCoordinate = legalCoordinate[0];
+    let occupiedCells = [];
+    let allNull = (arr) => arr.every((coordinate) => coordinate === null);
     if (direction === "horizontal") {
+      for (let j = 0; j < ship.length; j++) {
+        occupiedCells.push(this.grid[yCoordinate][xCoordinate + j]);
+      }
+      if (allNull(occupiedCells)) {
+        for (let i = 0; i < ship.length; i++) {
+          this.grid[yCoordinate][xCoordinate + i] = ship;
+        }
+      } else throw Error("You can't place ship on occupied cell");
+
+      for (let i = 0; i < ship.length; i++) {
+        if (legalCoordinate[0] + 1 < 10) {
+          this.grid[legalCoordinate[0] + 1][legalCoordinate[1] + i] = "occupied";
+        }
+        if (legalCoordinate[0] - 1 >= 0) {
+          this.grid[legalCoordinate[0] - 1][legalCoordinate[1] + i] = "occupied";
+        }
+        if (legalCoordinate[1] + ship.length < 10) {
+          this.grid[legalCoordinate[0]][legalCoordinate[1] + ship.length] = "occupied";
+        }
+        if (legalCoordinate[1] - 1 >= 0) {
+          this.grid[legalCoordinate[0]][legalCoordinate[1] - 1] = "occupied";
+        }
+      }
+
       const maxShipLengthX = legalCoordinate[1] + ship.length;
       if (legalCoordinate[0] < 0 || legalCoordinate[0] > 10 || maxShipLengthX > 10) {
         throw Error("This move is out of bounds");
@@ -16,6 +46,31 @@ class GameBoard {
       }
     }
     if (direction === "vertical") {
+      for (let j = 0; j < ship.length; j++) {
+        occupiedCells.push(this.grid[yCoordinate + j][xCoordinate]);
+      }
+      if (allNull(occupiedCells)) {
+        for (let i = 0; i < ship.length; i++) {
+          this.grid[yCoordinate + i][xCoordinate] = ship;
+        }
+        this.ships.push(ship);
+      } else throw Error("You can't place ship on occupied cell");
+
+      for (let i = 0; i < ship.length; i++) {
+        if (legalCoordinate[1] + 1 < 10) {
+          this.grid[legalCoordinate[0] + i][legalCoordinate[1] + 1] = "occupied";
+        }
+        if (legalCoordinate[1] - 1 >= 0) {
+          this.grid[legalCoordinate[0] + i][legalCoordinate[1] - 1] = "occupied";
+        }
+        if (legalCoordinate[0] + ship.length < 10) {
+          this.grid[legalCoordinate[0] + ship.length][legalCoordinate[1]] = "occupied";
+        }
+        if (legalCoordinate[0] - 1 >= 0) {
+          this.grid[legalCoordinate[0] - 1][legalCoordinate[1]] = "occupied";
+        }
+      }
+
       const maxShipLengthY = legalCoordinate[0] + ship.length;
       if (legalCoordinate[1] < 0 || legalCoordinate[1] > 10 || maxShipLengthY > 10) {
         throw Error("This move is out of bounds");
@@ -47,7 +102,7 @@ class GameBoard {
   receiveAttack(ship, coordinate) {
     let xCoordinate = coordinate[0];
     let yCoordinate = coordinate[1];
-    if (this.grid[xCoordinate][yCoordinate]) {
+    if (this.grid[xCoordinate][yCoordinate] instanceof Ship) {
       ship.hit();
       ship.isItSunk();
       return true;
