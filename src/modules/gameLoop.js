94c25612ce7aec1030bb821ab0395df71debd00b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-01-25 12:35:16 +0100

    Made several changes to gameLoop module
    Added, and made several changes to my GameLoop module including method
    for creating players, initiating computer game board, starting the game,
    random placing computer ships on game board, playing a game against a CPU .
    
    Also, I've removed an option and logic for playing vs. another player from
    my project.

--- src/modules/gameLoop.js
+++ src/modules/gameLoop.js
@@ -1,73 +1,42 @@
 import { CPU, Player } from "./Player";
 import {
   addBoardEventListeners,
-  appendStartingScreen,
+  assignDataAttributesToShips,
   changeColorCell,
+  dragDrop,
   getBoardValue,
   renderComputerMove,
+  renderPlayerShipsOnTheGameBoard,
+  resetBoardShipPlacement,
+  rotateShip,
 } from "./helperDOMMethods";
 
 import UI from "./UI";
 
 export function gameLoop() {
-  const playAgainstButtons = document.querySelectorAll(".play-against-button");
-  playAgainstButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      switch (e.target.getAttribute("value")) {
-        case "Computer":
-          console.log("computer");
-          appendStartingScreen(e.target.value);
-          playTheGame();
-          break;
-        case "Player":
-          appendStartingScreen(e.target.value);
-          playTheGame();
-          break;
-      }
-    });
+  const playAgainstButtons = document.querySelector(".player-create-button");
+  playAgainstButtons.addEventListener("click", () => {
+    startTheGame();
   });
 }
 
 export function createPlayers() {
-  if (document.querySelector("h3").textContent === "ENTER NAME OF THE PLAYER :") {
-    const playerNameInput = document.querySelector("#input-player-name");
+  const playerNameInput = document.querySelector("#input-player-name");
 
-    const humanPlayer = new Player(`${playerNameInput.value}`, true);
-    const CPUPlayer = new CPU("CPU", false);
+  const humanPlayer = new Player(`${playerNameInput.value}`, true);
+  const CPUPlayer = new CPU("CPU", false);
 
-    Player.playersArr.push(humanPlayer, CPUPlayer);
-  } else {
-    const playerNameInputPlayerOne = document.querySelector("#input-player-name");
-    const playerNameInputPlayerTwo = document.querySelector("#input-player-two-name");
-
-    const humanPlayerOne = new Player(`${playerNameInputPlayerOne.value}`, true);
-    const humanPlayerTwo = new Player(`${playerNameInputPlayerTwo.value}`, false);
-
-    Player.playersArr.push(humanPlayerOne, humanPlayerTwo);
-  }
+  Player.playersArr.push(humanPlayer, CPUPlayer);
 }
 
-export function initializeGameBoards() {
-  if (document.querySelector(".modal").classList.contains("cpu-game")) {
-    const humanPlayer = Player.playersArr[0];
-    const CPUPlayer = Player.playersArr[1];
-
-    UI.createBoards(humanPlayer.board.grid, "humanBoard");
-    UI.createBoards(CPUPlayer.board.grid, "CPUBoard");
-    placeShipsOnBoard([]);
-    addBoardEventListeners();
-  } else {
-    const humanPlayerOne = Player.playersArr[0];
-    const humanPlayerTwo = Player.playersArr[1];
-
-    UI.createBoards(humanPlayerOne.board.grid, "humanBoard");
-    UI.createBoards(humanPlayerTwo.board.grid, "humanBoard-two");
-    addBoardEventListeners();
-  }
+export function initializeComputerGameBoard() {
+  const CPUPlayer = Player.playersArr[1];
+  UI.createBoards(CPUPlayer.board.grid, "CPUBoard");
+  placeComputerShipsOnBoard([]);
+  addBoardEventListeners();
 }
 
-export function placeShipsOnBoard(arr) {
-  const player1 = Player.playersArr[0];
+export function placeComputerShipsOnBoard(arr) {
   const player2 = Player.playersArr[1];
   if (player2.name === "CPU") {
     const CPUPlayer = Player.playersArr[1];
@@ -96,25 +65,24 @@ export function checkForWinner() {
   const player2 = Player.playersArr[1];
 
   if (player1.board.allSunk()) {
-    alert("You won");
+    console.log("Computer has won!");
   } else if (player2.board.allSunk()) {
-    alert("COMP WON");
+    console.log("You Won!");
   }
 }
 
-export function playTheGame() {
-  document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    createPlayers();
-    initializeGameBoards();
-  });
-}
-
-export function playAgainstAnotherPlayer() {
-  document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    createPlayers();
-  });
+export function startTheGame() {
+  createPlayers();
+  document.querySelector(".modal").remove();
+  const humanPlayer = Player.playersArr[0];
+  UI.createBoards(humanPlayer.board.grid, "humanBoard");
+  UI.playAndResetButtons();
+  document.querySelector(".main-content").appendChild(UI.placableShips());
+  assignDataAttributesToShips();
+  rotateShip();
+  dragDrop();
+  resetBoardShipPlacement();
+  playAgainstCPU();
 }
 
 export function playerTurn(player, column, row, e) {
@@ -135,4 +103,12 @@ export function computerTurn() {
   CPUPlayer.randomAttack(pickRandomShip);
   renderComputerMove();
   checkForWinner();
+}
+
+export function playAgainstCPU() {
+  document.querySelector(".play-game-button").addEventListener("click", () => {
+    document.querySelector("#ship-container").remove();
+    initializeComputerGameBoard();
+    renderPlayerShipsOnTheGameBoard();
+  });
 }
