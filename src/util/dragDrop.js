b6276cfb9d6c4176811e5320ece2e5a47e6ec68c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-01-26 22:47:16 +0100

    added drag-hover class to my droppable element so that I can add hover effect when hovering over my gameboard

--- src/util/dragDrop.js
+++ src/util/dragDrop.js
@@ -1,7 +1,8 @@
 import { Player } from "../modules/Player";
-import { removeEventListeners } from "../modules/helperDOMMethods";
+import { removeEventListeners, showPlayGameButton } from "../modules/helperDOMMethods";
 
 export function dragEnter(e) {
+  e.target.classList.add("drag-hover");
   e.preventDefault();
 }
 
@@ -9,11 +10,14 @@ export function dragOver(e) {
   e.preventDefault();
 }
 
-export function dragLeave(e) {}
+export function dragLeave(e) {
+  e.target.classList.remove("drag-hover");
+}
 
 export function drop(e) {
   const id = e.dataTransfer.getData("text/plain");
   const draggable = document.getElementById(id);
+
   const playerShip = Player.createPlayerShip(Number(draggable.dataset.length));
   draggable.setAttribute("data-column", e.target.dataset.column);
   draggable.setAttribute("data-row", e.target.dataset.row);
@@ -23,13 +27,14 @@ export function drop(e) {
       [Number(e.target.dataset.column), Number(e.target.dataset.row)],
       draggable.dataset.orientation
     );
-    console.log(Player.playersArr[0].board.grid);
-  } catch (e) {
-    console.log(e);
-    return e;
+  } catch (error) {
+    e.target.classList.remove("drag-hover");
+    return error;
   }
-  console.log(Player.playersArr[0].board.grid);
   e.target.appendChild(draggable);
+  e.target.classList.remove("drag-hover");
+
+  showPlayGameButton();
 
   draggable.classList.add("dropped");
   draggable.style.position = "absolute";
