aba77faa049eabb459762016d850575f7e42e217
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-01-26 22:49:16 +0100

    added winner screen  left and right container for my UI elements  and also small drag and drop info paragraph text. And play Again button at the end of the game

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -3,18 +3,21 @@ const toHtml = require("../util/toHtml");
 export default class UI {
   static startingModalAgainstComputer = toHtml(`
   <dialog open class = "modal cpu-game" id = "modal">
-  <div class="player-creation-container">
-<h3>ENTER NAME OF THE PLAYER :</h3>
+<h3>ENTER PLAYER NAME:</h3>
 <form>
-<input type="text" id = "input-player-name" required  />
-<button class="player-create-button" type="submit">PLAY</button>
-</div>
+<input type="text" id = "input-player-name" required placeholder="Player Name" />
+<button class="player-create-button" type="submit">Play</button>
 </form>
 </dialog>
 `);
 
+  static dragAndDropInfo = toHtml(`    <div class = "drag-drop-info-container">
+<p> In order to start playing, please drag and drop all of your ships on the board. </p>
+</div>`);
+
   static placableShips = () => {
-    return toHtml(`<div id = ship-container>
+    return toHtml(`
+    <div id = ship-container>
   <div class = "ship horizontal" id = "carrier" draggable="true">
   <div class = "part"></div>
   <div class = "part"></div>
@@ -46,8 +49,12 @@ export default class UI {
   };
 
   static createBoards = (board, type) => {
+    const leftContainer = document.createElement("div");
     const gameBoard = document.createElement("div");
+    leftContainer.classList.add("left-container");
     gameBoard.classList.add("gameboard");
+    document.querySelector(".main-content").appendChild(leftContainer);
+    leftContainer.appendChild(document.querySelector(".gameboards-container"));
     document.querySelector(".gameboards-container").appendChild(gameBoard);
 
     board.forEach((column, j) => {
@@ -71,6 +78,14 @@ export default class UI {
       }
     });
   };
+
+  static rightContainerCreation = () => {
+    const rightContainer = document.createElement("div");
+    rightContainer.classList.add("right-container");
+
+    return rightContainer;
+  };
+
   static playAndResetButtons = () => {
     const buttonsContainer = document.createElement("div");
     const playGameButton = document.createElement("button");
@@ -82,10 +97,32 @@ export default class UI {
 
     playGameButton.textContent = "Play The Game";
     resetBoardButton.textContent = "Reset Board";
-    document.querySelector(".main-content").appendChild(buttonsContainer);
+
+    document.querySelector(".right-container").appendChild(buttonsContainer);
     buttonsContainer.appendChild(playGameButton);
     buttonsContainer.appendChild(resetBoardButton);
 
     return buttonsContainer;
   };
+
+  static winnerScreen = (name) => {
+    const dialog = document.createElement("dialog");
+    dialog.setAttribute("open", "");
+    const dialogContainer = document.createElement("div");
+    const paraWinStatus = document.createElement("p");
+    dialogContainer.classList.add("dialog-container");
+    paraWinStatus.textContent = `${name} has won the game`;
+
+    dialog.appendChild(dialogContainer);
+    dialogContainer.appendChild(paraWinStatus);
+
+    return dialog;
+  };
+
+  static createPlayAgainButton() {
+    const playAgainButton = document.createElement("button");
+    playAgainButton.classList.add("play-again-button");
+    playAgainButton.textContent = "Play Again";
+    document.querySelector("dialog").appendChild(playAgainButton);
+  }
 }
